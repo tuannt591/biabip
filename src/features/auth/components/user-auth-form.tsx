0cb973f2aeaf17b52b237f213eeb12560deebd42
nguyentuan <developer@ermis.network>
2025-09-17 17:34:33 +0700

    fix regex phone

--- src/features/auth/components/user-auth-form.tsx
+++ src/features/auth/components/user-auth-form.tsx
@@ -19,7 +19,7 @@ import * as z from 'zod';
 import OtpForm from './otp-form';
 
 const phoneRegex = new RegExp(
-  /^(\+?84|0)?(3[2-9]|5[6|8|9]|7[0|6-9]|8[1-5,9]|89|9[0-9])[0-9]{7}$/
+  /^(\+?84|0)?(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-9]|9[0-9])[0-9]{7}$/
 );
 
 const formatPhoneNumber = (phone: string): string => {
